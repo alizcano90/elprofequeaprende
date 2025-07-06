b427fd6350d417e20c411f8d1990c96be4e66f04
Anfaliz1703 <aflizcano@changeamericas.com>
2025-07-05 22:38:03 -0500

    Prueba deploy automático

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,15 @@
+<?php
+
+// Archivo: home.php
+// Guardado: 2025-07-05 22:37:58
+// Autor: Andrés Lizcano
+?>
+<p>¡Deploy automático en acción!</p>
+
 <div class='p-5 mb-4 bg-light rounded-3'>
-  <div class='container py-5'>
-    <h1 class='display-5 fw-bold'>Bienvenido a El Profe Que Aprende</h1>
-    <p class='col-md-8 fs-4'>Comparte y descarga recursos offline para docentes, descubre proyectos y mucho más.</p>
-    <a class='btn btn-primary btn-lg' href='?page=resources'>Ver Recursos</a>
-  </div>
+    <div class='container py-5'>
+        <h1 class='display-5 fw-bold'>Bienvenido a El Profe Que Aprende</h1>
+        <p class='col-md-8 fs-4'>Comparte y descarga recursos offline para docentes, descubre proyectos y mucho más.</p>
+        <a class='btn btn-primary btn-lg' href='?page=resources'>Ver Recursos</a>
+    </div>
 </div>
